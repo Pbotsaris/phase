5b36783a41e6f012917d9b12faa8296f112be3f4
Pbotsaris <pedro@antfood.com>
2022-01-05 13:38:47 +0100

    added basic delay tempo change

--- src/components/PhaseStage.svelte
+++ src/components/PhaseStage.svelte
@@ -5,10 +5,16 @@
   export let sequenceReady
 
   let cutoffFreq = 300;
+  let delayTime =  sequencer.getSynthsDelayTime('synth')
 
   function setCutoff(cutoffFreq){
     sequencer.setSynthsCutoff(cutoffFreq)
   }
+
+	function handleClick(whichSynth, time) {
+	sequencer.setSynthsDelayTime(whichSynth, time)
+	delayTime =  sequencer.getSynthsDelayTime('synth');
+	}
 
   $: setCutoff(cutoffFreq);
 
@@ -17,9 +23,18 @@
 <div class="phase-container">
   <StopButton {sequencer} bind:sequenceReady />
 
-  <input type="range" min="50" max="1000" bind:value={cutoffFreq} class="slider" >
-  {cutoffFreq}
+  <div>
+    <input type="range" min="50" max="1000" bind:value={cutoffFreq} class="slider" >
+    {cutoffFreq}
+  </div>
 
+		<div class="delay-time">
+  	<div class="selection" on:click={()=> {handleClick('synth', 0.5)}}> 4 </div>
+		<div class="selection"on:click={()=> {handleClick('synth', 0.25)}}> 8 </div>
+		<div class="selection"on:click={()=> {handleClick('synth', 0.125)}}> 16 </div>
+	</div>
+
+   {delayTime}
 </div>
 
 <style>
@@ -30,6 +45,27 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
+}
+
+.delay-time{
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+}
+
+.selection{
+display: flex;
+justify-content: center;
+align-items: center;
+margin: 0.2rem;
+
+width: 35px;
+height: 30px;
+cursor: pointer;
+
+background-color: darkcyan;
+color: white;
 }
 
 </style>
--- src/tone/sequencer.js
+++ src/tone/sequencer.js
@@ -1,50 +1,59 @@
-import * as Tone from 'tone';
+import * as Tone from 'tone'
 
 class Sequencer {
   constructor(synth, phasedSynth) {
-    this.synth = synth;
-    this.phasedSynth = phasedSynth;
-    this.sequence;
-    this.phasedSequence;
-    this.tranport = Tone.Transport;
-    this.delay = 0.0001;
+    this.synth = synth
+    this.phasedSynth = phasedSynth
+    this.sequence
+    this.phasedSequence
+    this.tranport = Tone.Transport
+    this.delay = 0.0001
 
-    this.synth.setPan(-0.2);
-    this.phasedSynth.setPan(0.2);
+    this.synth.setPan(-0.2)
+    this.phasedSynth.setPan(0.2)
   }
 
   create(notes) {
     this.sequence = new Tone.Sequence((time, note) => {
-      this.synth.noteOnTime(note, '8n', time);
-    }, notes);
+      this.synth.noteOnTime(note, '8n', time)
+    }, notes)
 
     this.phasedSequence = new Tone.Sequence((time, note) => {
-      this.phasedSynth.noteOnTime(note, '8n', time + this.delay);
-      this.delay += 0.0004;
-    }, notes);
+      this.phasedSynth.noteOnTime(note, '8n', time + this.delay)
+      this.delay += 0.0004
+    }, notes)
   }
 
   start() {
-    Tone.Transport.bpm.value = 140;
-    this.sequence.start(0);
-    this.phasedSequence.start(0);
-    Tone.Transport.start();
+    Tone.Transport.bpm.value = 140
+    this.sequence.start(0)
+    this.phasedSequence.start(0)
+    Tone.Transport.start()
   }
 
   stop() {
     this.sequence.stop()
     this.phasedSequence.stop()
-    this.sequence.dispose();
-    this.phasedSequence.dispose();
-    Tone.Transport.stop();
-    this.delay = 0.0001;
+    this.phasedSequence.dispose()
+    this.sequence.dispose()
+    Tone.Transport.stop()
+    this.delay = 0.0001
+  }
+
+  getSynthsDelayTime(whichSynth) {
+    const synth = whichSynth == 'phased' ? this.phasedSynth : this.synth
+    return synth.delay.delayTime.value
   }
 
   setSynthsCutoff(freq) {
-    this.synth.setFilterCutoff(freq);
-    this.phasedSynth.setFilterCutoff(freq);
+    this.synth.setFilterCutoff(freq)
+    this.phasedSynth.setFilterCutoff(freq)
   }
 
+  setSynthsDelayTime(whichSynth, delayTime) {
+    const synth = whichSynth == 'phased' ? this.phasedSynth : this.synth
+    synth.setDelay(delayTime, 0.3)
+  }
 }
 
-export default Sequencer;
+export default Sequencer
